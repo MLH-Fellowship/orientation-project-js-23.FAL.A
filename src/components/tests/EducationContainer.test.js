c82f372a5171963713b2911e6bbf864489a2c7b9
Juan Escalada <juanescalada175@gmail.com>
2023-09-21 18:32:31 -0700

    Add EducationContainer test

---
+++ src/components/tests/EducationContainer.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { EducationContainer } from "../EducationContainer";
+
+describe("EducationContainer component", () => {
+  it("renders without crashing", () => {
+    render(<EducationContainer />);
+  });
+
+  it("renders the Education header", () => {
+    render(<EducationContainer />);
+    const educationHeader = screen.getAllByText(/Education/i)[0];
+    expect(educationHeader).toBeInTheDocument();
+  });
+
+  it("renders the Add Education button", () => {
+    render(<EducationContainer />);
+    const addEducationButton = screen.getByRole("button", { name: /Add Education/i });
+    expect(addEducationButton).toBeInTheDocument();
+  });
+});
