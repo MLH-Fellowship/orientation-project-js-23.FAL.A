2d63a9d04eb2fa6c01a05b7b47bc2523c07e4813
Juan Escalada <juanescalada175@gmail.com>
2023-09-24 11:45:43 -0700

    Fix tests

--- src/components/ExperienceContainer/ExperienceContainer.test.js
+++ src/components/ExperienceContainer/ExperienceContainer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { ExperienceContainer } from "../ExperienceContainer";
+import { ExperienceContainer } from "./ExperienceContainer";
 
 describe("ExperienceContainer component", () => {
   it("renders without crashing", () => {
