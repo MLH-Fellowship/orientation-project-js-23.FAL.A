a090c8055bc2b3a996e3408a3a98dfc8f6f6caa2
Samson Amaugo <sammychinedu2ky@gmail.com>
2023-10-01 11:37:31 +0100

    fixed conflicts

--- src/components/EducationContainer/EducationContainer.test.js
+++ src/components/EducationContainer/EducationContainer.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { EducationContainer } from "./EducationContainer";
+import { EducationContainer } from "./EducationContainer.jsx";
 
 describe("EducationContainer component", () => {
   it("renders without crashing", () => {
